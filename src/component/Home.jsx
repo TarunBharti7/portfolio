8f153e17a9fa41a41b055079c1a6d1fdb82483d2
Tarun Bharti <tarunbharti970@gmail.com>
2023-10-13 21:16:55 +0530

    work  on responsive home and projects page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-        <section id='home'>
+      <section id='home'>
         <div className="container">
           <div className="info">
             <h1>Hii</h1>
-            <h1>I m Tarun, a</h1>
+            <h1>I'm Tarun, a</h1>
             <h1>Web Developer</h1>
             <a href="#project">
               <button>See My Project</button>
